4b3d1339b53621152fc9e96480f5f4abf319b8db
dariakoval <dasharibkavmore666@mail.ru>
2023-09-15 16:33:52 +0400

    update model Url.java

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -5,10 +5,14 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Getter
 @Setter
 @ToString
 public final class Url {
+
     private Long id;
 
     @ToString.Include
@@ -16,7 +20,18 @@ public final class Url {
 
     private Timestamp createdAt;
 
+    public Url(String name, Timestamp createdAt) {
+        this.name = name;
+        this.createdAt = createdAt;
+    }
+
     public Url(String name) {
         this.name = name;
+    }
+
+    public String getCreatedAtFormat() {
+        Date date = new Date(this.createdAt.getTime());
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        return formatter.format(date);
     }
 }
