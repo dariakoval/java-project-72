9c2888699f3a7133ccf62a4c46d74d1e96f03804
dariakoval <dasharibkavmore666@mail.ru>
2023-10-10 12:36:26 +0400

    create schema_psql.sql to generate db psql

---
+++ app/src/main/resources/schema_psql.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS urls CASCADE;
+
+CREATE TABLE urls (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
+
+DROP TABLE IF EXISTS url_checks;
+
+CREATE TABLE url_checks (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id BIGINT REFERENCES urls (id) NOT NULL,
+    status_code INTEGER,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP NOT NULL
+);
